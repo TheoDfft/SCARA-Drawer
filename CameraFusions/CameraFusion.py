2363295ef9ead6dbe5f531656a3d09b7a445ff9f
lorkol <37380984+lorkol@users.noreply.github.com>
2025-05-05 04:16:04 -0400

    Splitting the fusions into position and orientation, as well as the covariance matrices in advance

--- CameraFusions/CameraFusion.py
+++ CameraFusions/CameraFusion.py
@@ -10,14 +10,32 @@ Quaternion = Annotated[npt.NDArray[np.float64], (4,)]
 Pose = Annotated[npt.NDArray[np.float64], (7,)]
 Vector36D = Annotated[npt.NDArray[np.float64], (36,)]
 Matrix3x3 = Annotated[npt.NDArray[np.float64], (3, 3)]
+Matrix4x4 = Annotated[npt.NDArray[np.float64], (4, 4)]
 PoseCovariance = Annotated[npt.NDArray[np.float64], (6, 6)]
 
 
+def pose_fusion(pose1: Pose, pose2: Pose, covariance1: Vector36D, covariance2: Vector36D) -> Tuple[Pose, Vector36D]:
+    posecov1: PoseCovariance = covariance1.reshape(6, 6)
+    posecov2: PoseCovariance = covariance2.reshape(6, 6)
+    pos_cov1 = posecov1[:3, :3]
+    pos_cov2 = posecov2[:3, :3]
+    orientation_cov1 = posecov1[3:, 3:]
+    orientation_cov2 = posecov1[3:, 3:]
+    pos, pos_cov = position_fusion(pose1[:3], pose2[:3], pos_cov1, pos_cov2)
 
+    quaternion1: Quaternion = pose1[3:]
+    quaternion2: Quaternion = pose2[3:]
+    orientation, orientation_cov = orientation_fusion(quaternion1, quaternion2,
+                                                      orientation_cov1, orientation_cov2)
+
+    zeros: Matrix3x3 = np.zeros((3, 3))
+    covariance: Vector36D = np.block([[pos_cov, zeros],
+                                      [zeros, orientation_cov]]).flatten()
+    pose: Pose = np.concatenate(pos, orientation)
+    return pose, covariance
 
 def position_fusion(pos1: Position, pos2: Position,
-                    covariance1: Vector36D, covariance2: Vector36D) -> Tuple[Position, Matrix3x3]:
-    cov1, cov2 = _create_pos_covariances(covariance1, covariance2)
+                    cov1: Matrix3x3, cov2: Matrix3x3) -> Tuple[Position, Matrix3x3]:
 
     cov1_inv: Matrix3x3 = cholesky_inverse(cov1)
     cov2_inv: Matrix3x3 = cholesky_inverse(cov2)
@@ -36,13 +54,9 @@ def position_fusion(pos1: Position, pos2: Position,
     return pos, cov
 
 
-def _create_pos_covariances(covariance1, covariance2) -> Tuple[Matrix3x3, Matrix3x3]:
-    """Convert covariance matrices to position covariances only"""
-    posecov1: PoseCovariance = covariance1.reshape(6, 6)
-    posecov2: PoseCovariance = covariance2.reshape(6, 6)
-    cov1: Matrix3x3 = posecov1[:3, :3]
-    cov2: Matrix3x3 = posecov2[:3, :3]
-    return cov1, cov2
+def orientation_fusion(q1: Quaternion, q2: Quaternion,
+                       cov1: Matrix3x3, cov2: Matrix3x3) -> Tuple[Quaternion, Matrix3x3]:
+    pass
 
 
 def cholesky_inverse(mat: Matrix3x3) -> Matrix3x3:
